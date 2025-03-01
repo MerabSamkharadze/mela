c7488c5d92ed0cd3464b6e149f0f5b0a3a952f1a
Mebo <samkharadzemerab@gmail.com>
2025-03-01 15:15:22 +0400

    create api/courses and api/courses/[id] routes

---
+++ app/api/courses/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const supabase = await createClient();
+  const courseId = await params.id;
+
+  if (!courseId)
+    return NextResponse.json(
+      { error: "Course ID is required" },
+      { status: 400 }
+    );
+
+  const { data, error } = await supabase
+    .from("courses")
+    .select("title, cover_image, description, videos(video_url),created_at")
+    .eq("id", courseId)
+    .single();
+
+  if (error)
+    return NextResponse.json({ error: error.message }, { status: 500 });
+
+  return NextResponse.json(data);
+}
